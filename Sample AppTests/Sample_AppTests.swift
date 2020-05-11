af521ffcd03ca6de8c9a2ffdc68f29d168ed9d46
AlecDusheck <a.dusheckirl@gmail.com>
2020-05-11 16:19:08 -0500

    Initial Commit

---
+++ Sample AppTests/Sample_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sample_AppTests.swift
+//  Sample AppTests
+//
+//  Created by adusheck_eit on 5/11/20.
+//  Copyright © 2020 adusheck_eit. All rights reserved.
+//
+
+import XCTest
+@testable import Sample_App
+
+class Sample_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
