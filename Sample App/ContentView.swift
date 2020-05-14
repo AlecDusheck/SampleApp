8cde5ef63f92a9105ffbcb0e1cbb97b2a588ffab
AlecDusheck <a.dusheckirl@gmail.com>
2020-05-13 20:15:42 -0500

    Some changes to make my app awesome!!

--- Sample App/ContentView.swift
+++ Sample App/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        // No noe!!!
+        Text("Hello, World! This will be a conflict")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        // Just a comment
         ContentView()
     }
 }
